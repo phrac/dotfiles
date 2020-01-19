53297bf008b05fc9d76bea5e9a00149eef7ec703
Derek Musselmann <158748+phrac@users.noreply.github.com>
2020-01-19 11:42:34 -0600

    clean up dwm a bit

--- dwm/commands.c
+++ dwm/commands.c
@@ -16,21 +16,16 @@
 /* Standard commands */
 static char dmenumon[2] = "0";
 static const char *dmenucmd[]   =   { "rofi", "-show", "run", NULL };
-/*static const char *termcmd[]    =   { "/home/derek/dotfiles/st/st", NULL }; */
 static const char *termcmd[]    =   { "urxvtc", NULL};
-static const char *browsercmd[] =   { "iridium", NULL };
-static const char *screenshot[] =   { "/home/derek/bin/ss.py", NULL };
-static const char *windowshot[] =   { "/home/derek/bin/ss.py", "-window", NULL };
+static const char *browsercmd[] =   { "firefox", NULL };
+static const char *screenshot[] =   { "ss.py", NULL };
+static const char *windowshot[] =   { "ss.py", "-window", NULL };
 static const char *nexttrack[]  =   { "cmus-remote", "-n", NULL };
 static const char *prevtrack[]  =   { "cmus-remote", "-r", NULL };
 static const char *playpause[]  =   { "cmus-remote", "-u", NULL };
 static const char *thumbsup[]   =   { "cmus-remote", "-C", "win-add-p", NULL };
 static const char *editor[]     =   { "$EDITOR", NULL };
-static const char *guieditor[]  =   { "nvim-gtk", NULL };
+static const char *guieditor[]  =   { "emacs", NULL };
 static const char *fmcmd[]      =   { "thunar", NULL };
-static const char *termfm[]     =   { "/home/derek/dotfiles/st/st", "-e" "ranger", NULL };
+static const char *termfm[]     =   { termcmd, "-e" "ranger", NULL };
 static const char *browseremail[] = { "surf", "protonmail.com", NULL };
-static const char *decbright[]  =   { "xbacklight", "-dec", "10", "-steps", "1", NULL };
-static const char *incbright[]  =   { "xbacklight", "-inc", "10", "-steps", "1", NULL };
-
-
