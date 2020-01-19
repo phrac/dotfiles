0886d60aaee836dadad409b51102a24acf532134
Derek Musselmann <158748+phrac@users.noreply.github.com>
2020-01-19 09:48:45 -0600

    re-organize and add a few files

---
+++ config/.config/cmus/lib.pl
@@ -0,0 +1,213 @@
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 01 - Johnny Strikes Up the Band.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 02 - Roland the Headless Thompson Gunner.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 03 - Excitable Boy.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 04 - Werewolves of London.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 05 - Accidentally Like a Martyr.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 06 - Nighttime in the Switching Yard.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 07 - Veracruz.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 08 - Tenderness on the Block.flac
+/home/derek/Music/playlist/Excitable Boy (1978)/Warren Zevon - Excitable Boy - 09 - Lawyers, Guns and Money.flac
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/01 Beyond Good and Evil.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/03 The Break of Autumn.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/04 Non-Divine.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/05 Primal Breath.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/06 The Architects.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/07 Stardrowned.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/08 Blood of the Sunsets.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/09 The Burning Darkness.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/10 Ever-Opening Flower.mp3
+/home/derek/Music/playlist/At the Gates/With Fear I Kiss the Burning Darkness/11 Through the Red _ The Nightmare Continues.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/01 Blinded by Fear.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/02 Slaughter of the Soul.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/03 Cold.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/04 Under a Serpent Sun.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/05 Into the Dead Sky.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/06 Suicide Nation.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/07 World of Lies.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/08 Unto Others.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/09 Nausea.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/10 Need.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/11 The Flames of the End.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/01 - Blinded by Fear.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/02 - Slaughter of the Soul.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/03 - Cold.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/04 - Under a Serpent Sun.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/05 - Into the Dead Sky.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/06 - Suicide Nation.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/07 - World of Lies.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/08 - Unto Others.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/09 - Nausea.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/10 - Need.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/11 - The Flames of the End.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/12 - Legion.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/13 - The Dying.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/14 - Captor of Sin.mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/15 - Unto Others ('95 demo).mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/16 - Suicide Nation ('95 demo).mp3
+/home/derek/Music/playlist/At the Gates/Slaughter of the Soul/17 - Bister verklighet.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/01 - El altar del Dios desconocido.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/01 - El altar del Dios desconocido.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/01 El altar del Dios desconocido.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/01 El altar del Dios desconocido.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/02 - Death and the Labyrinth.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/02 - Death and the Labyrinth.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/02 Death and the Labyrinth.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/02 Death and the Labyrinth.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/03 - At War With Reality.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/03 - At War With Reality.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/03 At War With Reality.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/03 At War With Reality.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/04 - The Circular Ruins.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/04 - The Circular Ruins.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/04 The Circular Ruins.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/04 The Circular Ruins.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/05 - Heroes and Tombs.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/05 - Heroes and Tombs.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/05 Heroes and Tombs.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/05 Heroes and Tombs.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/06 - The Conspiracy of the Blind.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/06 - The Conspiracy of the Blind.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/06 The Conspiracy of the Blind.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/06 The Conspiracy of the Blind.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/07 - Order From Chaos.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/07 - Order From Chaos.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/07 Order From Chaos.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/07 Order From Chaos.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/08 - The Book of Sand (The Abomination).flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/08 - The Book of Sand (The Abomination).flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/08 The Book of Sand (The Abomination).flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/08 The Book of Sand (The Abomination).flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/09 - Language of the Dead.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/09 - Language of the Dead.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/09 Language of the Dead.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/09 Language of the Dead.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/10 - The Skin of a Fire.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/10 - The Skin of a Fire.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/10 The Skin of a Fire.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/10 The Skin of a Fire.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/11 - The Head of the Hydra.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/11 - The Head of the Hydra.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/11 The Head of the Hydra.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/11 The Head of the Hydra.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/12 - City of Mirrors.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/12 - City of Mirrors.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/12 City of Mirrors.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/12 City of Mirrors.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/13 - Eater of Gods.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/13 - Eater of Gods.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/13 Eater of Gods.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/13 Eater of Gods.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/14 - Upon Pillars of Dust.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/14 - Upon Pillars of Dust.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/14 Upon Pillars of Dust.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/14 Upon Pillars of Dust.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/15 - The Night Eternal.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/15 - The Night Eternal.flac.mp3
+/home/derek/Music/playlist/At the Gates/At War With Reality/15 The Night Eternal.flac
+/home/derek/Music/playlist/At the Gates/At War With Reality/15 The Night Eternal.flac.mp3
+/home/derek/Music/06+Seize+the+Day.mp3
+/home/derek/Music/playlist/Jake Owen - Down To The Honky Tonk.mp3
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01 Daft Punk Is Playing at My House.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/02 Too Much Love.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/03 Tribulations.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/04 Movement.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/05 Never as Tired as When I'm Waking Up.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/06 On Repeat.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/07 Thrills.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/08 Disco Infiltrator.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/09 Great Release.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/10 Losing My Edge.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/11 Beat Connection.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/12 Give It Up.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/13 Tired.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/14 Yeah (crass version).flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/15 Yeah (pretentious version).flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/16 Yr City's a Sucker (full version).flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-01 - Daft Punk Is Playing at My House.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-02 - Too Much Love.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-03 - Tribulations.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-04 - Movement.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-05 - Never as Tired as When I'm Waking Up.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-06 - On Repeat.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-07 - Thrills.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-08 - Disco Infiltrator.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/01-09 - Great Release.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/02-01 - Losing My Edge.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/02-02 - Beat Connection.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/02-03 - Give It Up.flac
+/home/derek/Music/playlist/LCD Soundsystem/LCD Soundsystem/02-04 - Tired.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/01 Get Innocuous!.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/02 Time to Get Away.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/03 North American Scum.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/04 Someone Great.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/05 All My Friends.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/06 Us v Them.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/07 Watch the Tapes.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/08 Sound of Silver.flac
+/home/derek/Music/playlist/LCD Soundsystem/Sound of Silver/09 New York, I Love You but Youre Bringing Me Down.flac
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/01 Dance Yrself Clean.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/02 Drunk Girls.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/03 One Touch.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/04 All I Want.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/05 I Can Change.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/06 You Wanted a Hit.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/07 Pow Pow.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/08 Somebodys Calling Me.mp3
+/home/derek/Music/playlist/LCD Soundsystem/This Is Happening/09 Home.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/01 - Oh Baby.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/01 - Oh Baby.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/02 - Other Voices.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/02 - Other Voices.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/03 - I Used To.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/03 - I Used To.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/04 - Change Yr Mind.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/04 - Change Yr Mind.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/05 - How Do You Sleep_.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/05 - How Do You Sleep_.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/06 - Tonite.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/06 - Tonite.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/07 - Call the Police.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/07 - Call the Police.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/08 - American Dream.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/08 - American Dream.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/09 - Emotional Haircut.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/09 - Emotional Haircut.flac.mp3
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/10 - Black Screen.flac
+/home/derek/Music/playlist/LCD Soundsystem/American Dream/10 - Black Screen.flac.mp3
+/home/derek/Music/playlist/I Want It All/01 Intro.flac
+/home/derek/Music/playlist/I Want It All/02 Gangsta Love.flac
+/home/derek/Music/playlist/I Want It All/03 Why Oh Why.flac
+/home/derek/Music/playlist/I Want It All/04 Dollars Make Sense.flac
+/home/derek/Music/playlist/I Want It All/05 I Want It All.flac
+/home/derek/Music/playlist/I Want It All/06 Havin Things.flac
+/home/derek/Music/playlist/I Want It All/07 You Never Know.flac
+/home/derek/Music/playlist/I Want It All/08 My Momma (Ola Mae).flac
+/home/derek/Music/playlist/I Want It All/09 G-Spot.flac
+/home/derek/Music/playlist/I Want It All/10 We Got That.flac
+/home/derek/Music/playlist/I Want It All/11 Dope Beat.flac
+/home/derek/Music/playlist/I Want It All/12 World Wide Ryders.flac
+/home/derek/Music/playlist/I Want It All/13 Game Dont Wait.flac
+/home/derek/Music/playlist/I Want It All/14 If We Give You a Chance.flac
+/home/derek/Music/playlist/I Want It All/15 I Want It All (remix).flac
+/home/derek/Music/playlist/I Want It All/16 Outro.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/01 Intro Shhhh.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/02 We Need Each Other.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/03 Make It Do What It Do.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/04 In Case Some Sh$# Go Down.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/05 I Need a Light.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/06 Get U Down.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/07 A Chronic Break.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/08 Weed Song.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/09 Wheels Keep Spinning.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/10 PYT.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/11 Walk These Streets.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/12 Garilla Pimpin.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/13 Turn It Up Loud.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/14 Mid-Nite Hour.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/15 I Like Dat There.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/16 Yessir.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/17 Ahh.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/18 All I Ask of You.flac
+/home/derek/Music/playlist/In the Mid-Nite Hour/19 Get U Down (remix).flac
+/home/derek/Music/01+The+Love+That+You+Give.mp3
+/home/derek/Music/02+Victorious.mp3
