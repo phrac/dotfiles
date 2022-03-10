d190491e79a1b8800d97911623ff3ea65d51b421
Derek Musselmann <158748+phrac@users.noreply.github.com>
2022-03-09 18:54:07 -0600

    additional configs

---
+++ emacs/.doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/notes/roam/20220222130857-robotic_grinding_cell_1.org" "/home/derek/notes/roam/todo.org"))
+ '(package-selected-packages '(arduino-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
