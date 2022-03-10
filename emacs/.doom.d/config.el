8bfe4151d4ccc94350148380fa8a057555ca6abc
Derek Musselmann <158748+phrac@users.noreply.github.com>
2022-03-10 06:08:55 -0600

    reformat config.el

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -48,29 +48,29 @@
 
 ;; org setting
 (setq org-directory "~/notes")
-;(setq org-agenda-files '("~/notes/roam/todo.org"))
+                                        ;(setq org-agenda-files '("~/notes/roam/todo.org"))
 (after! org
-;;  (add-to-list 'org-file-apps  '("\\.pdf" . "mupdf %s"))
+  ;;  (add-to-list 'org-file-apps  '("\\.pdf" . "mupdf %s"))
   (setq org-agenda-window-setup 'other-window)
   (setq org-id-locations-file "~/.doom.d/.state")
   (setq org-default-notes-file (concat org-directory "~/refile.org"))
   (setq org-capture-templates
-    '(("t" "New TODO" entry (file "~/notes/roam/todo.org")
-       "* TODO %?\n  %i\n %a")
-      ("w" "New Work Order" entry (file "~/notes/roam/todo.org")
-       "* W/O %?\n %i\n")
-      ("b" "Buy Item" entry (file "~/notes/roam/todo.org")
-       "* BUY %?\n  %i\n")
-      ("s" "Sell Item" entry (file "~/notes/roam/todo.org")
-       "* SELL %?\n  %i\n")
-      ("p" "Project" entry (file "~/notes/roam/todo.org")
-       "* PROJ %?\n  %i\n")
-      ("m" "Schedule Meeting" entry (file "~/notes/roam/todo.org")
-       "* MEET %?\n %i\n")
-      ("n" "NOTE" entry (file+olp+datetree "~/notes/roam/todo.org")
-       "* %?\nEntered on %U\n  %i %a")
-      ("h" "Habit" entry (file "~/notes/roam/todo.org")
-       "* HABIT %?\n%U\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
+        '(("t" "New TODO" entry (file "~/notes/roam/todo.org")
+           "* TODO %?\n  %i\n %a")
+          ("w" "New Work Order" entry (file "~/notes/roam/todo.org")
+           "* W/O %?\n %i\n")
+          ("b" "Buy Item" entry (file "~/notes/roam/todo.org")
+           "* BUY %?\n  %i\n")
+          ("s" "Sell Item" entry (file "~/notes/roam/todo.org")
+           "* SELL %?\n  %i\n")
+          ("p" "Project" entry (file "~/notes/roam/todo.org")
+           "* PROJ %?\n  %i\n")
+          ("m" "Schedule Meeting" entry (file "~/notes/roam/todo.org")
+           "* MEET %?\n %i\n")
+          ("n" "NOTE" entry (file+olp+datetree "~/notes/roam/todo.org")
+           "* %?\nEntered on %U\n  %i %a")
+          ("h" "Habit" entry (file "~/notes/roam/todo.org")
+           "* HABIT %?\n%U\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
   (setq org-todo-keywords
         '((sequence "TODO(t)" "NEXT(n@/!)" "PROJ(p@/!)" "WAIT(w@/!)" "ASSIGNED(a@/!)" "W/O(o@/!)" "MEET(m@/!)" "|" "DONE(d@/!)" "MET(M@/!)" "CANCELED(c@/!)")
           (sequence "BUY(b)" "|" "BOUGHT(B@/!)" "CANCELED(c@)")
@@ -111,153 +111,153 @@
   :after org-agenda
   :init
   (setq org-agenda-skip-scheduled-if-done t
-      org-agenda-skip-deadline-if-done t
-      org-agenda-include-deadlines t
-      ;org-agenda-block-separator nil
-      org-agenda-compact-blocks nil
-      org-agenda-start-day nil ;; i.e. today
-      org-agenda-span 1
-      org-agenda-start-on-weekday nil)
+        org-agenda-skip-deadline-if-done t
+        org-agenda-include-deadlines t
+                                        ;org-agenda-block-separator nil
+        org-agenda-compact-blocks nil
+        org-agenda-start-day nil ;; i.e. today
+        org-agenda-span 1
+        org-agenda-start-on-weekday nil)
   (setq org-agenda-custom-commands
         '(("c" "Super view"
            ((agenda "" ((org-agenda-overriding-header "")
                         (org-super-agenda-groups
                          '((:name "Today"
-                                  :time-grid t
-                                  :date today
-                                  :order 1)))))
+                            :time-grid t
+                            :date today
+                            :order 1)))))
             (alltodo "" ((org-agenda-overriding-header "")
                          (org-super-agenda-groups
                           '(
                             (:log t)
                             (:name "Due Today"
-                                   :date today
-                                   :order 1)
-                            (:name "To refile"
-                                   :file-path "refile\\.org")
+                             :date today
+                             :order 1)
                             (:name "Next Tasks"
-                                   :todo "NEXT"
-                                   :order 0)
+                             :todo "NEXT"
+                             :order 0)
                             (:name "On Hold"
-                                   :todo "WAIT"
-                                   :order 6)
+                             :todo "WAIT"
+                             :order 6)
                             (:name "Marketplace"
-                                   :todo ("BUY" "SELL" "LISTED")
-                                   :order 4)
+                             :todo ("BUY" "SELL" "LISTED")
+                             :order 4)
                             (:name "Assigned/Not Complete"
                              :todo ("ASSIGNED")
                              :order 5)
                             (:name "Important"
-                                   :priority "A"
-                                   :order 3)
-                            (:name "Low Effort Tasks"
-                                   :effort< "00:59")
-                            (:name "High Effort Tasks"
-                             :effort> "00:59")
+                             :priority "A"
+                             :order 3)
+                            (:name "Low Effort Tasks (< 1 hour)"
+                             :effort< "00:59")
+                            (:name "Medium Effort Tasks (1-3 hours)"
+                             :and (:effort> "00:59" :effort< "03:00"))
+                            (:name "High Effort Tasks (> 3 hours)"
+                             :effort> "03:00")
                             (:name "Upcoming Meetings"
                              :todo ("MEET")
                              :order 3)
                             (:name "Scheduled Soon"
-                                   :scheduled future
-                                   :order 3)
+                             :scheduled future
+                             :order 3)
                             (:name "Overdue"
-                                   :deadline past
-                                   :order 2)
+                             :deadline past
+                             :order 2)
                             (:name "Unfinished Projects"
-                                   :todo "PROJ"
-                                   :order 5)
+                             :todo "PROJ"
+                             :order 5)
                             (:discard (:not (:todo "TODO")))
                             ))))))
 
-        ("b" "Business Super view"
+          ("b" "Business Super view"
            ((agenda "" ((org-agenda-overriding-header "")
                         (org-super-agenda-groups
                          '((:name "Today"
-                                  :time-grid t
-                                  :and (:date today :file-path "business\\.org")
-                                  :order 1)))))
+                            :time-grid t
+                            :and (:date today :file-path "business\\.org")
+                            :order 1)))))
             (alltodo "" ((org-agenda-overriding-header "")
                          (org-super-agenda-groups
                           '(
                             (:log t)
                             (:name "Due Today"
-                                   :and (:date today :file-path "business\\.org")
-                                   :order 1)
+                             :and (:date today :file-path "business\\.org")
+                             :order 1)
                             (:name "To refile"
-                                   :file-path "refile\\.org"
-                                   :order 2)
+                             :file-path "refile\\.org"
+                             :order 2)
                             (:name "Upcoming Meetings"
-                                   :and (:todo "MEET" :file-path "business\\.org")
-                                   :order 2
-                                   )
+                             :and (:todo "MEET" :file-path "business\\.org")
+                             :order 2
+                             )
                             (:name "Work Orders"
-                                   :and (:todo "W/O" :file-path "business\\.org")
-                                   :order 2)
+                             :and (:todo "W/O" :file-path "business\\.org")
+                             :order 2)
                             (:name "On Hold"
-                                   :and (:todo "WAIT" :file-path "business\\.org")
-                                   :order 6)
+                             :and (:todo "WAIT" :file-path "business\\.org")
+                             :order 6)
                             (:name "Assigned Tasks"
-                                   :and (:todo "ASSIGNED" :file-path "business\\.org")
-                                   )
+                             :and (:todo "ASSIGNED" :file-path "business\\.org")
+                             )
                             (:name "Business Purchases"
-                                   :and (:todo "BUY" :file-path "business\\.org")
-                                   )
+                             :and (:todo "BUY" :file-path "business\\.org")
+                             )
                             (:name "Important"
-                                   :and (:priority "A" :file-path "business\\.org")
-                                   :order 3)
+                             :and (:priority "A" :file-path "business\\.org")
+                             :order 3)
                             (:name "Low Effort Tasks"
-                                   :and (:effort< "0:11" :file-path "business\\.org")
-                                   )
+                             :and (:effort< "0:11" :file-path "business\\.org")
+                             )
                             (:name "High Effort Tasks"
-                                   :and (:effort> "00:29" :file-path "business\\.org")
-                                   )
+                             :and (:effort> "00:29" :file-path "business\\.org")
+                             )
                             (:name "Scheduled Soon"
-                                   :and (:scheduled future :file-path "business\\.org")
-                                   :order 3)
+                             :and (:scheduled future :file-path "business\\.org")
+                             :order 3)
                             (:name "Overdue"
-                                   :and (:deadline past :file-path "business\\.org")
-                                   :order 0)
+                             :and (:deadline past :file-path "business\\.org")
+                             :order 0)
                             (:name "Meetings"
-                                   :and (:todo "MEET" :scheduled future :file-path "business\\.org")
-                                   :order 10)
+                             :and (:todo "MEET" :scheduled future :file-path "business\\.org")
+                             :order 10)
                             (:name "Unfinished Business Projects"
-                                   :and (:todo "PROJ" :file-path "business\\.org")
-                                   :order 5)
+                             :and (:todo "PROJ" :file-path "business\\.org")
+                             :order 5)
                             (:discard (:not(:file-path "business\\.org")))
                             ))))))
           ))
   :config
   (org-super-agenda-mode))
 (setq org-agenda-prefix-format '(
-  ;; (agenda  . " %i %-12:c%?-12t% s") ;; file name + org-agenda-entry-type
-  (agenda  . "• %i %-12:c%?-12t% s")
-  (timeline  . "• %i %-12:c%?-12t% s")
-  (todo  . " • %-12:c%?-12t% s")
-  (tags  . " • %-12:c%?-12t% s")
-  (search . " • %-12:c%?-12t% s")))
-;(use-package arduino-mode
-;            :init
-;             (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
-;        (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
-;        )
+                                 ;; (agenda  . " %i %-12:c%?-12t% s") ;; file name + org-agenda-entry-type
+                                 (agenda  . "• %i %-12:c%?-12t% s")
+                                 (timeline  . "• %i %-12:c%?-12t% s")
+                                 (todo  . " • %-12:c%?-12t% s")
+                                 (tags  . " • %-12:c%?-12t% s")
+                                 (search . " • %-12:c%?-12t% s")))
+                                        ;(use-package arduino-mode
+                                        ;            :init
+                                        ;             (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+                                        ;        (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
+                                        ;        )
 
 ;; org-roam settings
 ;;
 (setq org-roam-capture-templates '(
                                    ("p" "Project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
- :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
- :unnarrowed t)
+                                    :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
+                                    :unnarrowed t)
                                    ("d" "Default" plain "%?"
-  :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                     "#+title: ${title}\n#+category: ${title}\n")
-  :unnarrowed t)
+                                    :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                       "#+title: ${title}\n#+category: ${title}\n")
+                                    :unnarrowed t)
                                    ("c" "Contact" plain "%?"
-  :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                     "#+title: ${title}\n#+category: ${title}\n
+                                    :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                       "#+title: ${title}\n#+category: ${title}\n
                      \n* Details\n- Title:\n- Company:\n- Phone:\n- Email:\n")
-  :unnarrowed t)
+                                    :unnarrowed t)
                                    )
-)
+      )
 
 ;; Build agendas from org-roam files
 (defun my/org-roam-filter-by-tag (tag-name)
@@ -275,33 +275,32 @@
   (setq org-agenda-files (nconc
                           (my/org-roam-list-notes-by-tag "Project")
                           (my/org-roam-list-notes-by-tag "Tasks")))
-)
+  )
 
 ;; Build the agenda list the first time for the session
 (my/org-roam-refresh-agenda-list)
 
 ;; refile to roam targets
-  (setq myroamfiles (directory-files "~/notes/roam" t "org$"))
+(setq myroamfiles (directory-files "~/notes/roam" t "org$"))
 
-  ;; -------- refile settings -----
-  (setq org-refile-targets '((org-agenda-files :maxlevel . 5) (myroamfiles :maxlevel . 5)))
-  (setq org-refile-use-outline-path 'file)
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-refile-allow-creating-parent-nodes 'confirm)
+;; -------- refile settings -----
+(setq org-refile-targets '((org-agenda-files :maxlevel . 5) (myroamfiles :maxlevel . 5)))
+(setq org-refile-use-outline-path 'file)
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm)
 
 ;; org-roam-ui settings
 (use-package! websocket
-    :after org-roam)
+  :after org-roam)
 
 (use-package! org-roam-ui
-    :after org-roam ;; or :after org
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t))
-
+  :after org-roam ;; or :after org
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
